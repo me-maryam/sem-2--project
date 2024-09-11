8ce0014355ac49ff4872516f2a4132755b34665c
Maryam <162339097+me-maryam@users.noreply.github.com>
2024-09-11 15:36:08 +0500

    Add files via upload

---
+++ table.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Customer View</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Subject</th>
+                    <th>Message</th>
+                 
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($customers as $customer)
+               <tr>
+                <td>{{$customer->name}}</td>
+                <td>{{$customer->email}}</td>
+                <td>{{$customer->subject}}</td>
+                <td>{{$customer->message}}</td>
+                <td>
+                    @if ($customer->gender == 'M')
+                    Male
+                @elseif ($customer->gender == 'F')
+            Female
+            @elseif ($customer->gender == 'O')
+            Other
+            @endif
+            </td>
+                <td>{{$customer->points}}</td>
+                <td>
+                    @if ($customer->status == '1')
+                    Active
+                @else
+            Inactive
+            @endif
+            </td>
+               </tr>
+               @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
